038f5b1c5ddb9052b4dc4a2514d4d9b9feba439e
Adam McCoy <adam.mccoy31@gmail.com>
2021-09-06 15:31:02 -0600

    inil

--- my sql/Unit-12-MySQL-Homework-Employee-Tracker/config/orm.js
+++ my sql/Unit-12-MySQL-Homework-Employee-Tracker/config/orm.js
@@ -1,40 +1,25 @@
 let connection = require("./connection");
-
 function printQuestionMarks(num) {
   var arr = [];
-
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
-
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
+     arr.push(key + "=" + value);
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
-
 let orm = {
-
   all: (tableInput, cb) => {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
@@ -44,60 +29,47 @@ let orm = {
       cb(result);
     });
   },
-
   create: (table, cols, vals, cb) => {
     var queryString = "INSERT INTO " + table;
-
     queryString += " (";
     queryString += cols.toString();
     queryString += ") ";
     queryString += "VALUES (";
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
-
     console.log(queryString);
-
     connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
-
   update: (table, objColVals, condition, cb) => {
     var queryString = "UPDATE " + table;
-
     queryString += " SET ";
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
     queryString += condition;
-
     console.log(queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
-
   delete: (table, condition, cb) => {
     var queryString = "DELETE FROM " + table;
     queryString += " WHERE ";
     queryString += condition;
-
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
-
   employees: (col, cb) => {
     let queryString = `
     SELECT e.id,
@@ -123,7 +95,6 @@ let orm = {
       cb(result);
     });
   },
-
   roles: (cb) => {
     let queryString = "select id, title from role;";
     connection.query(queryString, (err, result) => {
@@ -133,7 +104,6 @@ let orm = {
       cb(result);
     });
   },
-
   managers: (cb) => {
     let queryString = "select * from manager;";
     connection.query(queryString, (err, result) => {
@@ -143,7 +113,6 @@ let orm = {
       cb(result);
     });
   },
-
   insert_employee: (firstName, lastName, roleId, managerId, cb) => {
     let queryString = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${roleId}, ${managerId});`;
     connection.query(queryString, function(err, result) {
@@ -153,7 +122,6 @@ let orm = {
       cb(result);
     });
   },
-
   update_employee_role: (roleId, employeeId, cb) => {
     let queryString = `UPDATE employee set role_id = ${roleId} where id = ${employeeId};`;
     connection.query(queryString, function(err, result) {
@@ -163,7 +131,6 @@ let orm = {
       cb(result);
     });
   },
-
   update_employee_manager: (managerId, employeeId, cb) => {
     let queryString = `UPDATE employee set manager_id = ${managerId} where id = ${employeeId};`;
     connection.query(queryString, function(err, result) {
@@ -173,8 +140,6 @@ let orm = {
       cb(result);
     });
   },
-
-
   delete_employee: (id, cb) => {
     let queryString = `delete from employee where id = ${id};`;
     connection.query(queryString, function(err, result) {
@@ -185,5 +150,4 @@ let orm = {
     });
   }
 };
-
 module.exports = orm;
