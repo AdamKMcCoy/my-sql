767932b1a5e50327d8d465b34fe583534621ca0f
Adam McCoy <adam.mccoy31@gmail.com>
2021-09-06 15:35:17 -0600

    inil

--- my sql/Unit-12-MySQL-Homework-Employee-Tracker/db/seed.sql
+++ my sql/Unit-12-MySQL-Homework-Employee-Tracker/db/seed.sql
@@ -1,10 +1,8 @@
-
 INSERT INTO department (name) VALUES ('Engineering');
 INSERT INTO department (name) VALUES ('Sales');
 INSERT INTO department (name) VALUES ('Finance');
 INSERT INTO department (name) VALUES ('Legal');
 INSERT INTO department (name) VALUES ('Human Resources');
-
 INSERT INTO role (title, salary, department_id) VALUES ('Professional Skateboarder', '200000.00', 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Bootcamp Teacher', '200000.00', 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Freelance Developer', '70000.00', 1);
@@ -14,10 +12,8 @@ INSERT INTO role (title, salary, department_id) VALUES ('Barrista', '40000.00', 
 INSERT INTO role (title, salary, department_id) VALUES ('CEO', '60000.00', 3);
 INSERT INTO role (title, salary, department_id) VALUES ('IBM Technician', '80000.00', 4);
 INSERT INTO role (title, salary, department_id) VALUES ('LinkedIn Recruiter', '80000.00', 5);
-
 INSERT INTO manager (first_name, last_name, role_id) VALUES ('Robert', 'Gaerlan', 1);
 INSERT INTO manager (first_name, last_name, role_id) VALUES ('Dave', 'Black', 2);
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Robert', 'Gaerlan', 1, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Cyndi', 'Green', 1, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Larry', 'Redman', 2, 1);
